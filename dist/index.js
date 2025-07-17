1f6de2c459f92dd53527fce7d7ccd1c77683997e
AL07183522 <AL07183522@tecmilenio.mx>
2025-07-16 18:44:31 -0600

    con cambios

--- dist/index.js
+++ dist/index.js
@@ -31559,15 +31559,15 @@ const { Octokit } = __nccwpck_require__(9380);
 async function run() {
   try {
     const usuario = core.getInput('usuario', { required: true });
-    const repo = core.getInput('repo', { required: true });
+    const repositorio = core.getInput('repositorio', { required: true });
     const org_user = core.getInput('org_user', { required: true });
     const token = process.env.GITHUB_TOKEN;
 
-    core.info(`Buscando commits de ${usuario} en ${org_user}/${repo}...`);
+    core.info(`Buscando commits de ${usuario} en ${org_user}/${repositorio}...`);
     const octokit = new Octokit({ auth: token });
     const response = await octokit.rest.repos.listCommits({
       owner: org_user,
-      repo: repo,
+      repo: repositorio,
       author: usuario,
       per_page: 100
     });
@@ -31575,8 +31575,8 @@ async function run() {
     const commits = response.data || [];
     core.info(`Se encontraron ${commits.length} commits.`);
 
-    // Mostrar los primeros 5 commits
-    commits.slice(0, 5).forEach((commit, idx) => {
+    // Mostrar los primeros 10 commit
+    commits.slice(0, 10).forEach((commit, idx) => {
       core.info(`${idx + 1}. ${commit.sha}: ${commit.commit.message}`);
     });
 
